08e2da619af1adeddb495512e3ddd3d0f2372ace
Kiran Dhoke <dhokekiran98@gmail.com>
2025-03-19 15:31:49 +0530

    feat: add Vite configuration for server settings and allowed hosts

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  server: {
+    allowedHosts: ['starfish-app-ca2ju.ondigitalocean.app'], // Your DigitalOcean domain
+    host: '0.0.0.0', // Allow external access
+    port: 1337, // Ensure this matches your Strapi server port
+  },
+};
