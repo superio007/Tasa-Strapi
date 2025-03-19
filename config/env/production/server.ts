f17cbad416919937280c797640beca305a27eec4
Kiran Dhoke <dhokekiran98@gmail.com>
2025-03-19 15:21:40 +0530

    feat: enhance production server configuration with CORS settings and allowed hosts

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,8 +1,16 @@
-// path: /config/env/production/server.ts
 export default ({ env }) => ({
-     proxy: true,
-     url: env('APP_URL'), // Sets the public URL of the application.
-     app: { 
-       keys: env.array('APP_KEYS')
-     },
- });
+  proxy: true,
+  url: env('APP_URL'), // Sets the public URL of the application.
+  app: { 
+    keys: env.array('APP_KEYS')
+  },
+  settings: {
+    cors: {
+      origin: ['*'], // Allow all origins (not recommended for production)
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    },
+  },
+  allowedHosts: [
+    'starfish-app-ca2ju.ondigitalocean.app', // Add your DigitalOcean app URL here
+  ],
+});
