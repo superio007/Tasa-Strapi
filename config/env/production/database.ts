b64fa82a1a56c6940e77cc85ce8ee23da01cb89b
Kiran Dhoke <dhokekiran98@gmail.com>
2025-03-19 16:23:32 +0530

    feat: update database configuration to use DATABASE_URL and add pg-connection-string dependency

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,17 +1,24 @@
- // path: /config/env/production/database.ts
- export default ({ env }) => ({
-   connection: {
-     client: 'postgres',
-     connection: {
-       host: env('DATABASE_HOST'), 
-       port: env.int('DATABASE_PORT'), 
-       database: env('DATABASE_NAME'), 
-       user: env('DATABASE_USERNAME'), 
-       password: env('DATABASE_PASSWORD'),
-       ssl: {
-         rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-       },
-     },
-     debug: false,
-   },
- });
+// path: /config/env/production/database.ts
+
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+   process.env.DATABASE_URL
+ );
+
+export default ({ env }) => ({
+  connection: {  
+    client: 'postgres',
+    connection: {
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        ca: env('DATABASE_CA'),
+      },
+    },
+    debug: false,
+  },
+});
