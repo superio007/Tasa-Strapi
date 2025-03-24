e877e76a08fb42d714aa4578a496d0957c87bf97
Kiran Dhoke <dhokekiran98@gmail.com>
2025-03-24 18:23:46 +0530

    feat: add sticky-form-heading API with controller, router, and service

---
+++ src/api/sticky-form-heading/controllers/sticky-form-heading.ts
@@ -0,0 +1,7 @@
+/**
+ * sticky-form-heading controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sticky-form-heading.sticky-form-heading');
---
+++ src/api/sticky-form-heading/services/sticky-form-heading.ts
@@ -0,0 +1,7 @@
+/**
+ * sticky-form-heading service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sticky-form-heading.sticky-form-heading');
